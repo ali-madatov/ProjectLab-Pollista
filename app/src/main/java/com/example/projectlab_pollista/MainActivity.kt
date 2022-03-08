64b75c50ebaa76929ec1d308fb9dae5f4d0a6ae7
ali-madatov <ali.madatov@hotmail.com>
2022-03-08 07:10:18 +0100

    Sign Up Page added

--- app/src/main/java/com/example/projectlab_pollista/MainActivity.kt
+++ app/src/main/java/com/example/projectlab_pollista/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.projectlab_pollista
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Contacts
 import android.view.View
+import com.example.projectlab_pollista.UI.SignUpActivity
+
+const val EXTRA_MESSAGE = "com.example.projectlab_pollista.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +16,10 @@ class MainActivity : AppCompatActivity() {
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         setContentView(R.layout.activity_getstarted)
 
+    }
+
+    fun signUp(view: View){
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/projectlab_pollista/UI/SignUpActivity.kt
@@ -0,0 +1,20 @@
+package com.example.projectlab_pollista.UI
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.projectlab_pollista.MainActivity
+import com.example.projectlab_pollista.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        setContentView(R.layout.activity_sign_up)
+    }
+    fun backToGetStarted(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
