4908838d62534985102578eef08e13b765393d21
ali-madatov <ali.madatov@hotmail.com>
2022-03-08 03:17:59 +0100

    Get Started page added

--- app/src/main/java/com/example/projectlab_pollista/MainActivity.kt
+++ app/src/main/java/com/example/projectlab_pollista/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.projectlab_pollista
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        setContentView(R.layout.activity_getstarted)
+
     }
 }
