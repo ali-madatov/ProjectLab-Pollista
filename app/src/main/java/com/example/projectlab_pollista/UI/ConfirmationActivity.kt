886aa3503ff0755878008a1bffa43927133d3d4d
ali-madatov <ali.madatov@hotmail.com>
2022-03-08 11:40:39 +0100

    Confirmation Page Added

---
+++ app/src/main/java/com/example/projectlab_pollista/UI/ConfirmationActivity.kt
@@ -0,0 +1,21 @@
+package com.example.projectlab_pollista.UI
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.projectlab_pollista.R
+
+class ConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        setContentView(R.layout.activity_confirmation)
+    }
+
+    fun signIn(view: View){
+        val intent = Intent(this,SignInActivity::class.java)
+        startActivity(intent)
+    }
+}
